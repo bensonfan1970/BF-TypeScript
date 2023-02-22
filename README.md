48ec833e6bbed4806c3b13a402deaddc118320be
Benson Fan <bfan@bfans-MacBook-Pro.local>
2023-02-21 17:11:24 -0800

    added hellow world sample

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BF-TypeScript
+
+cd helloworld
+tsc app.ts
+app.ts will be converted to app.js
+open index.html in a browser to verify
---
+++ helloworld/app.ts
@@ -0,0 +1,7 @@
+let bfMsg: string = 'Hello, TypeScript!';
+console.log(bfMsg)
+
+let heading = document.createElement('h1');
+heading.textContent = bfMsg;
+
+document.body.appendChild(heading);
